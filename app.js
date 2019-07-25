2ca9c67f4b6b2a1c81b49ddd1c75238de2c7ecff
Gildon Kirk <gildonkirk@gmail.com>
2019-07-25 12:43:19 -0400

    paypal recognizes price of one item

--- app.js
+++ app.js
@@ -1,47 +1,4 @@
 addToCart();
-paypal.Button.render({
-  // Configure environment
-  env: 'sandbox',
-  client: {
-    sandbox: 'AevBNj4gVlvmDY-Sb0FjIYiP2xd3tuJURLLlXfNeIfP1BNmH7G26TSQwMPBZp_uhuipLNcUBDs_RMp7R',
-    production: 'demo_production_client_id'
-  },
-  // Customize button (optional)
-  locale: 'en_US',
-  style: {
-    size: 'small',
-    color: 'gold',
-    shape: 'pill',
-  },
-
-  // Enable Pay Now checkout flow (optional)
-  commit: true,
-
-  // Set up a payment
-  payment: function(data, actions) {
-    console.log(actions.payment);
-    return actions.payment.create({
-      transactions: [{
-        amount: {
-          total: '0.01',
-          currency: 'USD'
-        }
-      }]
-    }).then(function(response) {
-      console.log(response);
-      return response; //Pay ID
-    });
-  },
-  // Execute the payment
-  onAuthorize: function(data, actions) {
-    console.log(data);
-    console.log(actions);
-    return actions.payment.execute().then(function() {
-      // Show a confirmation message to the buyer
-      window.alert('Thank you for your purchase!');
-    });
-  }
-}, '#paypal-button');
 
 function addToCart() {
   $(document).on('click', '.listItem', function() {
@@ -49,7 +6,6 @@ function addToCart() {
     console.log(newItem.parent().children('p').text());
     let newItemPrice = newItem.parent().children('p').text();
     newItemPrice = newItemPrice.replace('$', '');
-    newItemPrice = parseInt(newItemPrice);
     console.log(newItemPrice);
     $('.chosenItems').append($(this));
     $(this).addClass('cartItem');
@@ -57,5 +13,49 @@ function addToCart() {
       console.log('add button');
       $('.shoppingCart').append('<button type="button" name="checkout">Checkout</button>');
     };
+
+    paypal.Button.render({
+      // Configure environment
+      env: 'sandbox',
+      client: {
+        sandbox: 'AevBNj4gVlvmDY-Sb0FjIYiP2xd3tuJURLLlXfNeIfP1BNmH7G26TSQwMPBZp_uhuipLNcUBDs_RMp7R',
+        production: 'demo_production_client_id'
+      },
+      // Customize button (optional)
+      locale: 'en_US',
+      style: {
+        size: 'small',
+        color: 'gold',
+        shape: 'pill',
+      },
+
+      // Enable Pay Now checkout flow (optional)
+      commit: true,
+
+      // Set up a payment
+      payment: function(data, actions) {
+        console.log(actions.payment);
+        return actions.payment.create({
+          transactions: [{
+            amount: {
+              total: newItemPrice,
+              currency: 'USD'
+            }
+          }]
+        }).then(function(response) {
+          console.log(response);
+          return response; //Pay ID
+        });
+      },
+      // Execute the payment
+      onAuthorize: function(data, actions) {
+        console.log(data);
+        console.log(actions);
+        return actions.payment.execute().then(function() {
+          // Show a confirmation message to the buyer
+          window.alert('Thank you for your purchase!');
+        });
+      }
+    }, '#paypal-button');
   });
 };
